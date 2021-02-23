8929d38401a50dd745f4fbd42f66e7a97a0e8e6a
Pavel <pavelfedorov929@gmail.com>
2021-02-23 13:19:23 +0300

    Сделаны шаги профиля

--- dist/js/script.js
+++ dist/js/script.js
@@ -11,7 +11,7 @@ $(function () {
     });
 
     $('.header-dashboard__item-title').on('click', function () {
-        $(this).parent().parent().find('ul').slideToggle('300');
+        $(this).parent().find('ul').slideToggle('300');
     });
 
     $('.dashboard__item-header').on('click', function () {
@@ -23,6 +23,30 @@ $(function () {
     });
 
     $('.filter-style').styler();
+
+    $('input[name=phone]').mask("+375 (99) 999-99-99");
+
+    $('#email-enter').on('click', function () {
+        $(this).css('display', 'none');
+        $('.enter-popup__phone-enter').css('display', 'none');
+        $('.enter-popup__email-enter').css('display', 'block');
+    });
+
+    /* function maskPhone() {
+        var country = $('#country option:selected').val();
+        switch (country) {
+            case "ru":
+                $("input[name=phone]").mask("+7(999) 999-99-99");
+                break;
+            case "ua":
+                $("input[name=phone]").mask("+380(999) 999-99-99");
+                break;
+            case "by":
+                $("input[name=phone]").mask("+375(999) 999-99-99");
+                break;
+        }
+    }
+    maskPhone(); */
 
     /*  */
 
