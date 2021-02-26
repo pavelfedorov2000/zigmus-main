2532b858be6d3d997e2b0dd9e967548dc27f9190
Pavel <pavelfedorov929@gmail.com>
2021-02-26 16:12:47 +0300

    Страница с выполнения заказа готова

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,6 +1,14 @@
-$('[name=password]').change(function(){
+$('[name=password]').change(function () {
     console.log(this.value)
 });
-$('[name=repeat-password]').change(function(){
+$('[name=repeat-password]').change(function () {
     console.log(this.value)
 });
+
+/* @@include('../blocks/modules/header/header.js') */
+
+/* @@include('tabs.js') */
+
+/* @@include('scroll.js') */
+//@@include('validate.js')
+//>>>>>>> dfff71b8346c3ff47422372b2e25bbbc89db27af
