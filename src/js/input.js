919e741303b22c66eb608ee9700fcd99c93e28ca
Pavel <pavelfedorov929@gmail.com>
2021-03-16 03:26:33 +0300

    Поправил добавление сотрудников. Сделал грузы. Добавил габариты в транспорте

--- src/js/input.js
+++ src/js/input.js
@@ -15,3 +15,28 @@ $('.radio-field__label').on('click', function () {
 $('.oval-check').on('click', function () {
     $(this).find('span').toggleClass('checked');
 });
+
+$('.gabarits__minus-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.parent().siblings();
+    var value = parseInt($input.val());
+
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 1;
+    }
+
+    $input.val(value);
+
+});
+
+$('.gabarits__plus-btn').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.parent().siblings();
+    var value = parseInt($input.val());
+    value = value + 1;
+    $input.val(value);
+});
