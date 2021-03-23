309bf10a89dc7c43244521248f18c3a3c80db3e1
Roman Timonin <roman.timonin.1998@icloud.com>
2021-03-23 23:52:35 +0300

    Начал делать страницу апрув заказа

--- dist/js/script.js
+++ dist/js/script.js
@@ -297,17 +297,17 @@ $("#phone-enter").on("click", function () {
 
 $('.from-label').on('click', function () {
     $('.overlay, #map-popup').fadeIn('slow');
-    $("body").addClass('body__overflow--hidden');
+    // $("body").addClass('body__overflow--hidden');
 });
 
 $('.where-label').on('click', function () {
     $('.overlay, #map-popup').fadeIn('slow');
-    $("body").addClass('body__overflow--hidden');
+    // $("body").addClass('body__overflow--hidden');
 });
 
-$('#map-popup').on('change', function () {
-    $("body").removeClass('body__overflow--hidden');
-});
+// $('#map-popup').on('change', function () {
+//     $("body").removeClass('body__overflow--hidden');
+// });
     
     $('.from-label').on('click', function () {
     $('.overlay, #map-popup').fadeIn('slow');
